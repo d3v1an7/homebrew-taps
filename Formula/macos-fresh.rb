8b9d195857d4da9f53cb1e4ff252cefa92b1f79a
Ben White <hello@bemm.com.au>
2022-12-12 10:33:00 +1100

    Update macos-fresh.rb

--- Formula/macos-fresh.rb
+++ Formula/macos-fresh.rb
@@ -2,9 +2,9 @@ class MacosFresh < Formula
     include Language::Python::Virtualenv
     desc 'macos-fresh'
     homepage 'https://github.com/d3v1an7/macos-fresh'
-    url 'https://github.com/d3v1an7/macos-fresh/releases/download/0.22.0/macos_fresh-0.22.0.tar.gz'
-    sha256 '49f95a1f8f7ed230bc3c3b74718562900e7f8c5c77bf7787118fd57a1b9647b0'
-    version '0.22.0'
+    url 'https://github.com/d3v1an7/macos-fresh/releases/download/0.1.0/macos_fresh-0.1.0.tar.gz'
+    sha256 '88dfe33657953b96b77e9df7660793b260f8026ec7ac366036800e6acf2f712a'
+    version '0.1.0'
     depends_on 'python@3.11'
     def install
       venv = virtualenv_create(libexec, 'python3')
