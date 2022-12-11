f8c9f41fc490c63838b815616eb57600ce681d5f
Ben White <hello@bemm.com.au>
2022-12-12 10:15:08 +1100

    macos-fresh 0.22.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/macos-fresh.rb
+++ Formula/macos-fresh.rb
@@ -2,9 +2,9 @@ class MacosFresh < Formula
     include Language::Python::Virtualenv
     desc 'macos-fresh'
     homepage 'https://github.com/d3v1an7/macos-fresh'
-    url 'https://github.com/d3v1an7/macos-fresh/releases/download/0.21.0/macos_fresh-0.21.0.tar.gz'
-    sha256 '944595e14f413f5a67d5a1f0d2cde31096715dc027d143b57cab919c0da1e81f'
-    version '0.21.0'
+    url 'https://github.com/d3v1an7/macos-fresh/releases/download/0.22.0/macos_fresh-0.22.0.tar.gz'
+    sha256 '49f95a1f8f7ed230bc3c3b74718562900e7f8c5c77bf7787118fd57a1b9647b0'
+    version '0.22.0'
     depends_on 'python@3.11'
     def install
       venv = virtualenv_create(libexec, 'python3')
