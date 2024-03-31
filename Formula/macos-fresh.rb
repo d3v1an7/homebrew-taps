acc3a3217c69b0d3eb03f117bddf164beb61bb19
Ben White <hello@bemm.com.au>
2024-03-31 22:14:15 +1100

    macos-fresh 0.6.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/macos-fresh.rb
+++ Formula/macos-fresh.rb
@@ -2,9 +2,9 @@ class MacosFresh < Formula
     include Language::Python::Virtualenv
     desc 'macos-fresh'
     homepage 'https://github.com/d3v1an7/macos-fresh'
-    url 'https://github.com/d3v1an7/macos-fresh/releases/download/0.5.0/macos_fresh-0.5.0.tar.gz'
-    sha256 '6c8dfd84d2ffd56280dfd7e67166c37da3e4082d957f8c766eb7ed3f7ae55711'
-    version '0.5.0'
+    url 'https://github.com/d3v1an7/macos-fresh/releases/download/0.6.0/macos_fresh-0.6.0.tar.gz'
+    sha256 '24c02586d7cf610d1a67af932ac35e67a1e0a25cbc5de143a225a9e75d0fe9db'
+    version '0.6.0'
     depends_on 'python@3.11'
     def install
       venv = virtualenv_create(libexec, 'python3')
