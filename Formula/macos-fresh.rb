279243952099eb2017721a78ad914170f5948a98
Ben White <hello@bemm.com.au>
2026-04-24 18:42:36 +1000

    macos-fresh: rewrite formula for Node bundle (1.0.3)

--- Formula/macos-fresh.rb
+++ Formula/macos-fresh.rb
@@ -1,17 +1,21 @@
 class MacosFresh < Formula
-    include Language::Python::Virtualenv
-    desc 'macos-fresh'
-    homepage 'https://github.com/d3v1an7/macos-fresh'
-    url 'https://github.com/d3v1an7/macos-fresh/releases/download/1.0.3/macos-fresh-1.0.3.tar.gz'
-    sha256 '45ffcffbc556e3a21ffa1001d3fbb809691c521b9f222f41a852d463a445bcab'
-    version '1.0.3'
-    depends_on 'python@3.11'
-    def install
-      venv = virtualenv_create(libexec, 'python3')
-      system libexec/'bin/pip', 'install', '-v', '--ignore-installed', buildpath
-      bin.install_symlink libexec/'bin/fresh' => 'fresh'
-    end
-    test do
-      false
-    end
+  desc "Super opinionated tools, apps and defaults for macOS"
+  homepage "https://github.com/d3v1an7/macos-fresh"
+  url "https://github.com/d3v1an7/macos-fresh/releases/download/1.0.3/macos-fresh-1.0.3.tar.gz"
+  sha256 "45ffcffbc556e3a21ffa1001d3fbb809691c521b9f222f41a852d463a445bcab"
+  license "GLWTPL"
+  depends_on "node"
+
+  def install
+    libexec.install "dist/fresh.mjs"
+    (bin/"fresh").write <<~EOS
+      #!/bin/sh
+      exec "#{Formula["node"].opt_bin}/node" "#{libexec}/fresh.mjs" "$@"
+    EOS
+    chmod 0755, bin/"fresh"
   end
+
+  test do
+    assert_match "Usage:", shell_output("#{bin}/fresh --help")
+  end
+end
