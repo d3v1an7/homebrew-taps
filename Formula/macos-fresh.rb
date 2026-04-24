8bcd7a34bc0edc97956e73b1d5bf13c6b720ca93
Ben White <hello@bemm.com.au>
2026-04-24 19:43:39 +1000

    macos-fresh 1.0.5
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/macos-fresh.rb
+++ Formula/macos-fresh.rb
@@ -1,8 +1,8 @@
 class MacosFresh < Formula
   desc "Super opinionated tools, apps and defaults for macOS"
   homepage "https://github.com/d3v1an7/macos-fresh"
-  url "https://github.com/d3v1an7/macos-fresh/releases/download/1.0.3/macos-fresh-1.0.3.tar.gz"
-  sha256 "45ffcffbc556e3a21ffa1001d3fbb809691c521b9f222f41a852d463a445bcab"
+  url "https://github.com/d3v1an7/macos-fresh/releases/download/1.0.5/macos-fresh-1.0.5.tar.gz"
+  sha256 "694f116df704db6d15ca8b3ad265d04575d3d0e0abb72f1a7fe37c2cfcb3085c"
   license "GLWTPL"
   depends_on "node"
 
